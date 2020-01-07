4e3f403202ca3428596bcc86cb3efb9aa88c5f37
Ben England <bengland@redhat.com>
2020-01-07 15:34:21 -0500

    make CI more reliable
    centralize benchmark image update in update_benchmark_image()
    exit with failure unless all image update commands succeed
    use sudo if not run from root
    delete any pre-existing image before uploading new one
    simplify check_es
    edit operator.yaml before building operator image

--- ci/common.sh
+++ ci/common.sh
@@ -2,26 +2,68 @@
 
 es_server="marquez.perf.lab.eng.rdu2.redhat.com"
 es_port=9200
+image_account=${RIPSAW_CI_IMAGE_ACCOUNT:-rht_perf_ci}
+if [ "$USER" != "root" ] ; then
+  SUDO=sudo
+fi
+
+# process exit statuses
+OK=0
+NOTOK=1
+
+# this function rebuilds the benchmark image, it takes 3 parameters:
+# wrapper_dir - sub-directory where wrapper lives
+# tool - image name
+# tag name: defaults to "snafu_ci"
+
+function update_benchmark_image() {
+  wrapper_dir=$1
+  tool=$2
+  tag_name=${3:-snafu_ci}
+
+  image_spec=quay.io/$image_account/$tool:$tag_name
+
+  # make sure the pre-existing image is removed.  That way,
+  # if the image update fails and somehow isn't reflected in status code,
+  # CI test will still fail
+
+  skopeo inspect docker://$image_spec
+  if [ $? = $OK ] ; then 
+    skopeo delete docker://$image_spec || exit $NOTOK
+  fi
+
+  # rebuild the image, tag it and push it, 
+  # return failure status if any of these don't work
+  ($SUDO buildah build-using-dockerfile -f $wrapper_dir . 2>&1 | tee /tmp/buildah-bud.$$.log && \
+   image_id=`tail -n 1 /tmp/buildah-bud.$$.log` && \
+   $SUDO podman tag $image_id $image_spec && \
+   $SUDO podman push $image_spec) \
+       || exit $NOTOK
+}
 
 function update_operator_image() {
   tag_name=$1
-  operator-sdk build quay.io/rht_perf_ci/benchmark-operator:$tag_name --image-builder podman
+  sed -i "s|          image: quay.io/benchmark-operator/benchmark-operator:master*|          image:
+  quay.io/$image_account/benchmark-operator:$tag_name # |" resources/operator.yaml
+  $SUDO operator-sdk build quay.io/$image_account/benchmark-operator:$tag_name --image-builder podman || \
+    exit $NOTOK
 
   # In case we have issues uploading to quay we will retry a few times
   try_count=0
   while [ $try_count -le 2 ]
   do
-    if podman push quay.io/rht_perf_ci/benchmark-operator:$tag_name
-    then
-      try_count=2
-    elif [[ $try_count -eq 2 ]]
-    then
-      echo "Could not upload image to quay. Exiting"
-      exit 1
+    $SUDO podman push quay.io/$image_account/benchmark-operator:$tag_name
+    s=$?
+    if [ $s = $OK ] ; then
+        break
     fi
     ((try_count++))
   done
-  sed -i "s|          image: quay.io/benchmark-operator/benchmark-operator:master*|          image: quay.io/rht_perf_ci/benchmark-operator:$tag_name # |" resources/operator.yaml
+  if [ $try_count -gt 2 -a $s != $OK ]
+  then
+    echo "Could not upload image to repository. Exiting"
+    exit $NOTOK
+  fi
 }
 
 function wait_clean {
@@ -44,14 +86,8 @@ function check_es() {
   rc=0
   for my_index in $index
   do
-    python3 ci/check_es.py -s $es_server -p $es_port -u $uuid -i $my_index
-    ec=$?
-    if [[ $ec -ne 0 ]]
-    then
-      exit 1
-    fi
+    python3 ci/check_es.py -s $es_server -p $es_port -u $uuid -i $my_index || return $NOTOK
   done
-  exit 0
 }
 
 # Takes test script as parameter and returns the uuid
